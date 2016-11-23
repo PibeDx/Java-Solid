87d3a3722cd5c15acad66f9b3af09ec718e8a18a
José Cuentas <jose.c@osp.pe>
2016-11-23 16:01:58 -0500

    feat add module open_closed

---
+++ open_closed/src/main/java/good/FormalPersonality.java
@@ -0,0 +1,10 @@
+package good;
+
+/**
+ * Created by mrk on 4/7/14.
+ */
+public class FormalPersonality implements Personality {
+    public String greet() {
+        return "Good evening, sir.";
+    }
+}
